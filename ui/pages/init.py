c3d5fc51e07d5044d044fe0119078fabc95cb799
seckujdivad <David.a.juckes@tesco.net>
2018-09-02 16:18:09 +0100

    Added play button

--- ui/pages/init.py
+++ ui/pages/init.py
@@ -4,9 +4,12 @@ class Page:
     def __init__(self, tools):
         self.tools = tools
         
-        title_label = tk.Label(self.tools['frame'], text = 'Racer', **self.tools['styling']('large', 'label'))
+        label_title = tk.Label(self.tools['frame'], text = 'Racer', **self.tools['styling']('large', 'label'))
         
-        title_label.grid(row = 0, column = 0, sticky = 'NESW')
+        button_play = tk.Button(self.tools['frame'], text = 'Play', **self.tools['styling']('medium', 'button'))
+        
+        label_title.grid(row = 0, column = 0, sticky = 'NESW')
+        button_play.grid(row = 1, column = 0, sticky = 'NESW')
     
     def show(self):
         pass
